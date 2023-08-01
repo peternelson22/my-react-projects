d4461ec2d2638a183d58575da503d36045033c95
nelson <peternelson063@gmail.com>
2023-08-01 09:12:39 +0100

    Added tours

---
+++ tours/src/App.jsx
@@ -0,0 +1,59 @@
+import { useEffect } from 'react';
+import { useState } from 'react';
+import Tours from './Tours';
+import Loading from './Loading';
+
+const url = 'https://course-api.com/react-tours-project';
+function App() {
+  const [tours, setTours] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const removeTour = (id) => {
+    const newTours = tours.filter((tour) => tour.id !== id);
+    setTours(newTours);
+  };
+
+  const fetchTours = async () => {
+    setLoading(true);
+    try {
+      const res = await fetch(url);
+      const data = await res.json();
+      setTours(data);
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchTours();
+  }, []);
+
+  if (loading) {
+    return (
+      <main>
+        <Loading />;
+      </main>
+    );
+  }
+  if (tours.length === 0) {
+    return (
+      <main>
+        <div className='title'>
+          <h2>no tours left</h2>
+          <button className='btn' onClick={() => fetchTours()}>
+            refresh
+          </button>
+        </div>
+      </main>
+    );
+  }
+
+  return (
+    <main>
+      <Tours tours={tours} removeTour={removeTour} />
+    </main>
+  );
+}
+
+export default App;
